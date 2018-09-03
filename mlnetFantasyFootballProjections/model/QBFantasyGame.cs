e2093fbb667355463a7d12d60e0d4b7b121b4132
flizzer <bdavis@macpro.local>
2018-09-03 10:46:36 -0400

    adding props to game and prediction classes

--- mlnetFantasyFootballProjections/model/QBFantasyGame.cs
+++ mlnetFantasyFootballProjections/model/QBFantasyGame.cs
@@ -1,6 +1,95 @@
 using Microsoft.ML.Runtime.Api;
+using System;
 
 public class QBFantasyGame
 {
     [Column("0")]
+    public int GameNumber; 
+
+    [Column("1")]
+    public DateTime Date;
+    
+    [Column("3")]
+    public string Team;
+    
+    [Column("4")]
+    public string Opponent;
+    
+    [Column("5")]
+    public string Result;
+
+    [Column("6")]
+    public string Position;
+
+    [Column("7")]
+    public int Inside20PassCompletions;
+
+    [Column("8")]
+    public int Inside20PassAttempts;
+
+    [Column("9")]
+    public int Inside20PassYards;
+
+    [Column("10")]
+    public int Inside20PassTouchdowns;
+    
+    [Column("11")]
+    public int Inside20RushAttempts;
+
+    [Column("12")]
+    public int Inside20RushYards;
+
+    [Column("13")]
+    public int Inside20RushTouchdowns;
+
+    [Column("14")]
+    public int Inside10PassCompletions;
+
+    [Column("15")]
+    public int Inside10PassAttempts;
+
+    [Column("16")]
+    public int Inside10PassYards;
+
+    [Column("17")]
+    public int Inside10PassTouchdowns;
+
+    [Column("18")]
+    public int Inside10RushAttempts;
+
+    [Column("19")]
+    public int Inside10RushYards;
+
+    [Column("20")]
+    public int Inside10RushTouchdowns;
+
+    [Column("20")]
+    public int OffensiveSnapNumber;
+
+    [Column("21")]
+    public float OffensiveSnapPercentage {
+        get { return int.Parse(_OffensiveSnapPercentage.Trim('%')) / 100f; }
+    }
+    private string _OffensiveSnapPercentage;
+
+    [Column("22")]
+    public int DefensiveSnapNumber;
+
+    [Column("23")]
+    public float DefensiveSnapPercentage {
+
+        get { return int.Parse(_DefensiveSnapPercentage.Trim('%')) / 100f; }
+    }
+    private string _DefensiveSnapPercentage;
+
+    [Column("24")]
+    public int SpecialTeamsSnapNumber;
+
+    [Column("25")]
+    public float SpecialTeamsSnapPercentage {
+
+        get { return int.Parse(_SpecialTeamsSnapPercentage.Trim('%')) / 100f; }
+    }
+    private string _SpecialTeamsSnapPercentage;
+
 }
--- mlnetFantasyFootballProjections/model/QBFantasyGamePointsPrediction.cs
+++ mlnetFantasyFootballProjections/model/QBFantasyGamePointsPrediction.cs
@@ -1,1 +1,7 @@
 using Microsoft.ML.Runtime.Api;
+
+public class QBFantasyGamePointsPrediction 
+{
+    [ColumnName("FantPt")]
+    public float TotalFantasyPoints;
+}
